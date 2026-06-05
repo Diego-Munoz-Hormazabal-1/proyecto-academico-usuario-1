d6d4969ba21a9e27aa46b9b1dfb43efbaa3722f0
DiegMuñozH <dieg.munozh@duocuc.cl>
2026-06-05 18:59:34 -0400

    Actualizo el codigo con JWT

--- src/main/java/com/example/Usuario/repository/UsuarioRepository.java
+++ src/main/java/com/example/Usuario/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.example.Usuario.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.example.Usuario.model.entitie.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Agregamos este método para buscar por el email único
+    Optional<Usuario> findByEmail(String email);
 }
